c1fca17a9ffc88bff9355cd47c742967d2a2b024
patrick <patrickhartleyjr@gmail.com>
2017-05-04 23:25:49 -0500

    redo

--- app.js
+++ app.js
@@ -1,77 +1,61 @@
 var streams = ["casiodorus", "ESL_SC2", "comster404", "OgamingSC2", "witwix", "rorsheck", "riotgamesru", "FCCOKC"];
 var url = "";
 var game = "",
-    name = "",
-    viewers = "",
-    logoURL = "",
-    isOnline = false,
-    isActive = true,
-    html = "";
+displayName = "",
+viewers = "",
+logoURL = "",
+isOnline = false,
+isActive = true,
+html = "";
 
-streams.forEach(function (streamer) {
-	
-
-	//GET USER INFO
-	url = "https://wind-bow.gomix.me/twitch-api/channels/" + streamer;
-	$.ajax({
-		type: "GET",
-		url: url,
-		async: false,
-		dataType: "json",
-		success: function(user){
-			//GET NUMBER OF VIEWERS AND ONLINE STATUS
-			url = "https://wind-bow.gomix.me/twitch-api/streams/" + user.name;
-			
-			$.ajax({
-				type: "GET",
-				url: url,
-				async: false,
-				dataType: "json",
-				success: function(dude){
-					user.viewers = dude.stream.viewers;
-					user.isOnline = true;
-				}
-			});
-			if(!user.viewers){
-				//USER IS OFFLINE
-				user.viewers = 0;
-				user.isOnline = false;
-			}
-			displayStreamerInfo(user);
-		}
-	});
-
-	function displayStreamerInfo(data){
-		console.log(data.display_name + "!!!");
-		//DISPLAY USER'S PROFILE PICTURE
-		if(data.logo === undefined){
-			//USER DOES NOT EXIST!
-			data.isActive = false;
-			html += '<a href="https://www.twitch.tv/' + data.name + '" class="list-group-item disabled"><img class="profile-pic" src="';
-			html += 'https://img.clipartfest.com/5e9c58d9fa1aa44523a113686006795c_red-not-sign-transparent-clip-x-clipart-transparent_300-300.png">';
-		} else if(data.logo === null){
-			//USER HAD DEFAULT PROFILE PIC
-			html += '<a href="https://www.twitch.tv/' + data.name + '" class="list-group-item"><img class="profile-pic" src="';
-			html += 'https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png">';
-		} else {
-			//USER HAS CUSTOMER PROFILE PIC
-			html += '<a href="https://www.twitch.tv/' + data.name + '" class="list-group-item"><img class="profile-pic" src="';
-			html += data.logo + '"">';
-		}
-
-		//DISPLAY NAME, VIEWERS, AND ONLINE STATUS
-		html += '<h3>' + streamer + '</h3><span class="badge viewers-badge"><span class="glyphicon glyphicon-eye-open text-left">';
-		html += '</span>' + data.viewers + '</span><span class="status-badge badge">';
-		if(data.isOnline){
-			html += '<span class="glyphicon glyphicon-ok-sign"></span><p class="text-right">ONLINE</p></span>';
-			html += "<h5 class='text-center'>" + data.game + "</h5></a>";
-		} else{
-			html += '<span class="glyphicon glyphicon-remove-sign"></span><p class="text-right">OFFLINE</p></span></a>';
-		}
-		if(!data.isActive){
-			html += "<h5 class='text-center'>ERROR 404</h5></a>";
-		}
-	}
+streams.forEach(function (stream) {
+  function createURL(type, streamer){
+    return "https://wind-bow.gomix.me/twitch-api/" + type + "/" + streamer + '?callback=?';
+  }
+  var user = "";
+  //GET USER INFO
+  $.getJSON(createURL("streams",stream), function(data){
+    console.log("Success!");
+    if(data.stream === null){
+      //User is Offline
+      user.isOnline = false;
+      user.isActive = true;
+    }else if (data.stream ===undefined){
+      //User does not exist
+      user.isOnline = false;
+      user.isActive = false;
+      user.viewers = 0;
+    }else{
+      //User is Online
+      user.isOnline = true;
+      user.viewers = data.stream.viewers;
+    }
+    $.getJSON(createURL("channels", stream), function(data){
+      user.logoURL = data.logo != null ? data.logo : "https://img.clipartfest.com/5e9c58d9fa1aa44523a113686006795c_red-not-sign-transparent-clip-x-clipart-transparent_300-300.png";
+      user.displayName = data.display_name != null ? data.display_name : "ERROR 404";
+    });
+  });
+  displayStreamerInfo(user);
 });
 
-$("ul").append(html);
+
+function displayStreamerInfo(user){
+  console.log(user.displayName + "!!!");
+  //DISPLAY USER'S PROFILE PICTURE
+  html = '<a href="https://www.twitch.tv/' + user.displayName + '" class="list-group-item "><img class="profile-pic" src="';
+  html += user.logoURL + '">';
+
+  //DISPLAY NAME, VIEWERS, AND ONLINE STATUS
+  html += '<h3>' + user.displayName + '</h3><span class="badge viewers-badge"><span class="glyphicon glyphicon-eye-open text-left">';
+  html += '</span>' + user.viewers + '</span><span class="status-badge badge">';
+  if(user.isOnline){
+    html += '<span class="glyphicon glyphicon-ok-sign"></span><p class="text-right">ONLINE</p></span>';
+    html += "<h5 class='text-center'>" + user.game + "</h5></a>";
+  } else{
+    html += '<span class="glyphicon glyphicon-remove-sign"></span><p class="text-right">OFFLINE</p></span></a>';
+  }
+  if(!user.isActive){
+    html += "<h5 class='text-center'>ERROR 404</h5></a>";
+  }
+  $("ul").append(html);
+}
